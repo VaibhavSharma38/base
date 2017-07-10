4b746c57ae2b72f7e3297caf21591925e152fd50
vaibhav sharma <vaibhavsharma38@gmail>
2017-07-10 15:02:35 +0530

    Solved loading problem of country and state model if $_SERVER['SERVER_ADDR'] is there.

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -241,11 +241,11 @@ class Initiator extends \Controller_Addon {
                                 ->del('fields')->get();
                 // throw new \Exception(var_dump($s), 1);
                 // exit;
-                $ip_country= $this->add('xepan\base\Model_Country')->tryLoadBy('name',$s['country']); 
-                $ip_state = $this->add('xepan\base\Model_State')->tryLoadBy('name',$s['state']);
+                $ip_country= $this->add('xepan\base\Model_Country')->tryLoadBy('name',$s['0']['country']); 
+                $ip_state = $this->add('xepan\base\Model_State')->tryLoadBy('name',$s['0']['state']);
                 $this->app->memorize('xepan-customer-current-country',$ip_country);
                 $this->app->memorize('xepan-customer-current-state',$ip_state);
-            }
+            }            
             $this->app->country=$this->app->recall('xepan-customer-current-country');
             $this->app->state=$this->app->recall('xepan-customer-current-state');
         }
